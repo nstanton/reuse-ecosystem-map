4eb86517038ad87a5a9138f9d2701996f040420f
Nathan Stanton <nstanton@users.noreply.github.com>
2025-12-31 00:28:30 -0500

    Update to just show the map

--- src/index.js
+++ src/index.js
@@ -1,12 +1,8 @@
 import * as Data from './data.js'
 import * as Map from './map.js'
-import * as Table from './table.js'
 import './style/main.scss'
 
 const app = document.querySelector('#app')
-const parentUrl = (window.location != window.parent.location) ? document.referrer : document.location.href
-
-const hideTable = window.location.href.indexOf("table=hide") > 0
 
 app.innerHTML = `
 <div id="spinner">
@@ -15,19 +11,8 @@ app.innerHTML = `
   </div>
 </div>
 <div id="map"></div>
-<a href="https://www.buildingproductecosystems.org/" target="_blank">
-  <img src="images/bpe-logo.png" alt="Building Product Ecosystems" id="mapLogo"></img>
-</a>
 <div id="mapLegend"></div>
 <div id="regionSelectDiv"></div>
-
-<div class="credits" style="display: ${hideTable ? "none" : "block"}">
-  ${parentUrl.indexOf("buildingproductecosystems.org") > 0  ? "" : '<p>Resources compiled by <a href="https://www.buildingproductecosystems.org/">Building Product Ecosystems</a> | Please submit <a href="https://www.buildingproductecosystems.org/regional-reuse-resources#suggestions">suggestions, additions, or corrections</a> here.</p>'}
-</div>
-
-<div class="table-wrapper" style="display: ${hideTable ? "none" : "block"}">
-  <table id="table" class="stripe display responsive" width="100%"></table>
-</div>
 `
 
 const init = async () => {
@@ -39,7 +24,6 @@ const init = async () => {
   console.log(airtableData, colorsData);
 
   Map.init(airtableData, colorsData);
-  Table.init(airtableData, '#table');
 
   const mapLegend = document.querySelector('#mapLegend')
   mapLegend.innerHTML = Map.mapLegend(colorsData)
